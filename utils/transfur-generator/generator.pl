a0ef8055cc96e46008231c7841a91c8b536b67bc
KJEntytek303 <jakdom6@gmail.com>
2026-02-18 23:30:34 +0100

    Work on the generator and polishing of the example file

--- utils/transfur-generator/generator.pl
+++ utils/transfur-generator/generator.pl
@@ -1,2 +1,172 @@
 #!/usr/bin/perl
 
+use warnings;
+use strict;
+
+
+#global variables
+my $PACKAGE_PATH = '../../src/main/java/net/brown_bakers/bakers_transfurs/entity/generated';
+my $VERSION = '0.1';
+
+
+#pre-main {{{
+
+my @files;
+my @prepared_files;
+
+getlopt(@ARGV);
+my $argc = scalar(@files);
+
+#}}}
+
+
+#variables #{{{
+
+my $template = "./templates/generic.java.template";		#template file
+my $extend = "net.ltxprogrammer.changed.entity.ChangedEntity";	#which class to extend
+
+my @presets;
+my @attributes;
+my @abilities;
+my @scares;			#what mobs fear the variant
+my @imports;			#java class imports
+
+my $transfur_sound;		#sound a variant makes when transfurring
+my $transfur_mode;		#default tf mode
+my $mining_speed;
+my $legs;			#amount of legs
+my $entity_shape;		#entity shape enum. Assumes changed namespace, and is a subject to change
+my $show_hotbar;		#benigns have hidden hotbar
+my $items_in_main_hand;		#benings don't render items
+my $items_in_offhand;		#benigns and pups don't render items
+my $block_interaction;		#benigns can't interact
+my $block_breaking;		#benigns can't break blocks
+my $fly;
+my $jumps;			#jump charges
+my $vision;			#default vision type
+my $climb;			#stiger climb
+my $safe_fall;			#safe fall distance multiplier
+my $z_offset;			#camera z-offset used for taurs
+my $gendered;			#this switches the script from generating 1 file, to generating 3.
+my $freezing_ticks;		#powder snow
+my $breathing_mode;
+my $aqua_affinity;
+my $transfur_colors;
+#my $latex_faction;		#DL/WL swimming and cover slowdown
+
+my @registered_transfurs;
+my @final_imports;
+	
+#}}}
+
+#main
+
+#if @files is empty, recompile all entries in ./variants
+if ($argc == 0)
+{
+	@files = `ls -1 ./variants`;
+	foreach (@files) {
+		generateTransfur($_);
+	}
+
+}
+
+sub generateTransfur {
+	open
+	
+	
+}
+
+sub resetValues { #{{{
+	$template = "./templates/generic.java.template";		
+	$extend = "net.ltxprogrammer.changed.entity.ChangedEntity";	
+
+	@presets = ();
+	@attributes = ();
+	@abilities = ();
+	@scares = ();		
+	@imports = ();		
+
+	$transfur_sound = "";	
+	$transfur_mode = "";	
+	$mining_speed = "";
+	$legs = "";		
+	$entity_shape = "";	
+	$show_hotbar = "";	
+	$items_in_main_hand = "";	
+	$items_in_offhand = "";	
+	$block_interaction = "";	
+	$block_breaking = "";	
+	$fly = "";
+	$jumps = "";		
+	$vision = "";		
+	$climb = "";		
+	$safe_fall = "";		
+	$z_offset = "";		
+	$gendered = "";		
+	$freezing_ticks = "";	
+	$breathing_mode = "";
+	$aqua_affinity = "";
+	$transfur_colors = "";
+#	$latex_faction = "";
+
+} #}}}
+
+
+#Getopt and stuff {{{
+sub getlopt { #{{{
+	foreach (@ARGV){
+		if ($_ =~ /^--help$/) {
+			printHelp();
+			exit(0);
+		}
+		if ($_ =~ /^--version$/) {
+			print $VERSION;
+			exit(0);
+		}
+		if ($_ =~ /^--crash$/) {
+			$should_crash = 1;
+			next;
+		}
+		if ($_ =~ /^-[hcV]+$/) {
+			getsopt($_);
+			next;
+		}
+
+		#if not opt, push into files
+		push(@files, $_);
+	}
+} #}}}
+
+sub getsopt { #{{{
+	if ($_ =~ /h/) {
+		printHelp();
+		exit(0);
+	}
+	if ($_ =~ /V/) {
+		print $VERSION;
+		exit(0);
+	}
+	if ($_ =~ /c/) {
+		$should_crash = 1;
+	}
+} #}}}
+
+sub printHelp { #{{{
+	print "
+KJEntytek's303 Line Oriented Format Transfur Generator
+Version $VERSION
+
+USAGE:
+./generator.sh [OPTION] [FILE]
+
+If no file is given, the program recompiles all models inside ./variants
+
+OPTIONS:
+ -h	--help		- Displays this message
+ -c	--crash 	- Crashes on error
+ -V	--version	- Prints version
+";
+} #}}}
+
+#}}}
