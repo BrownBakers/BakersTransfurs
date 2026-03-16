c5879205d7a192e75b7d5c5b1fd7812ab2b91152
KJEntytek303 <jakdom6@gmail.com>
2026-03-16 18:09:42 +0100

    Finished BJCJ 2.0

--- utils/datagen/bjcj-2.0.pl
+++ utils/datagen/bjcj-2.0.pl
@@ -22,6 +22,16 @@ import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.world.entity.HumanoidArm;
 
+//Needed for taurs
+import net.ltxprogrammer.changed.client.tfanimations.HelperModel;
+import net.ltxprogrammer.changed.client.tfanimations.TransfurHelper;
+import net.ltxprogrammer.changed.client.animations.Limb;
+import net.ltxprogrammer.changed.client.renderer.animate.tail.WolfTailInitAnimator;
+
+//Needed for legless
+import net.ltxprogrammer.changed.client.renderer.model.LeglessModel;
+import net.ltxprogrammer.changed.entity.beast.LatexSnake;
+
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -38,13 +48,16 @@ my $GENERIC_OVERRIDES = '	@Override
 	@Override
 	public ModelPart getHead() { return this.Head; }
 
+	@Override
+	public ModelPart getArm(HumanoidArm humanoidArm) { return humanoidArm == HumanoidArm.LEFT ? this.LeftArm : this.RightArm; }
+
 	/*PERL_LEG_OVERRIDES*/
 	
 ';
 
-my $LEGLESS_IMPLEMENTS = "implements LeglessModel ";
+my $LEGLESS_IMPLEMENTS = "implements net.ltxprogrammer.changed.client.renderer.model.LeglessModel ";
 
-my $CENTAUR_IMPLEMENTS = "implements LowerTorsoedModel ";
+my $CENTAUR_IMPLEMENTS = "implements net.ltxprogrammer.changed.client.renderer.model.LowerTorsoedModel ";
 
 my $LEGLESS_LEG_OVERRIDES ='
 
@@ -60,7 +73,6 @@ my $LEGLESS_LEG_OVERRIDES ='
 	return super.getTransfurHelperModel(limb);
 	}
 
-	@Override
 	public boolean shouldModelSit(LatexSnake entity) { return super.shouldModelSit(entity) || LeglessModel.shouldLeglessSit(entity); }
 ';
 
@@ -95,12 +107,12 @@ my $MASKED = '	public boolean isPartNotMask(ModelPart part) { return Mask.getAll
 	';
 
 my $BIPED_LEG_OVERRIDES = '
-	@Override
-	public ModelPart getArm(HumanoidArm humanoidArm) { return humanoidArm == HumanoidArm.LEFT ? this.LeftArm : this.RightArm; }
 	
 	@Override
 	public ModelPart getLeg(HumanoidArm humanoidArm) { return humanoidArm == HumanoidArm.LEFT ? this.LeftLeg : this.RightLeg; }
 ';
+
+my $human_legs = 'LeftLeg, RightLeg';
 
 my $DECLARATIONS = "	private final HumanoidAnimator</*PERL_CAPITALIZED_NAME*/, /*PERL_CAPITALIZED_NAME*/Model> animator;
 
@@ -117,24 +129,30 @@ my $ANIMATOR_INIT = "		animator = HumanoidAnimator.of(this).hipOffset(-1.5f)
 					 /*PERL_NORMAL_TAIL_STUFF*/ 
 					 /*PERL_LEG_STUFF*/
 					 /*PERL_WINGED_STUFF*/
-					 ));
+					 ))/*PERL_ADDITIONAL_ANIMATORS*/;
 
 ";
 
-my $ears = "LeftEar, RightEar,"
+my $ears = "LeftEar, RightEar,";
 
-my $normal_tail_stuff = "Tail, List.of(/*PERL_TAIL_PARTS_ARRAY*/),"
+my $normal_tail_stuff = "Tail, List.of(/*PERL_TAIL_PARTS_ARRAY*/),";
 
-my $legless_abdomen = "Abdomen, LowerAbdomen,"
+my $legless_abdomen = "Abdomen, LowerAbdomen,";
 my $centaur_legs = '
-                LowerTorso, FrontLeftLeg, leftLowerLeg, LeftFoot, FrontRightLeg, rightLowerLeg, RightFoot,
-                BackLeftLeg, leftLowerLeg2, leftFoot2, LeftPad2, BackRightLeg, rightLowerLeg2, rightFoot2, RightPad2))
-                .addAnimator(new WolfTailInitAnimator<>(Tail, List.of(/*PERL_TAIL_PARTS_ARRAY*/)))
-                .forwardOffset(-7.0f).hipOffset(-1.5f).legLength(13.5f).torsoLength(11.05f);
-'
+                LowerTorso, LeftLeg, LeftLowerLeg, LeftFoot, RightLeg, RightLowerLeg, RightFoot,
+                LeftLeg, LeftLowerLeg2, LeftFoot2, LeftPad2, RightLeg2, RightLowerLeg2, RightFoot2, RightPad2
+';
 
-my $dragon_wings =
-my $bird_wings =
+my $centaur_additional_animators = '
+                .addAnimator(new WolfTailInitAnimator<>(Tail, List.of(/*PERL_TAIL_PARTS_ARRAY*/)))
+                .forwardOffset(-7.0f).hipOffset(-1.5f).legLength(13.5f).torsoLength(11.05f)
+';
+
+my $biped_legs = 'LeftLeg, LeftLowerLeg, LeftFoot, LeftPad, RightLeg, RightLowerLeg, RightFoot, RightPad';
+
+my $dragon_wings = ', leftWingRoot, leftSecondaries, leftTertiaries,
+                        rightWingRoot, rightSecondaries, rightTertiaries';
+my $bird_wings = ', LeftFlight, LeftSubFlight, RightFlight, RightSubFlight';
 
 my $input_file_name = '';
 my $name_capitalized ;
@@ -143,7 +161,7 @@ my $output_file_name = '';
 my $preset = '';
 my @tail_parts = ();
 my $is_masked = 0;
-my $leg_type = "";
+my $leg_type = "biped";
 
 getlopt(@ARGV);
 
@@ -156,13 +174,13 @@ $name_lowercase =~ s/([A-Z])/_$1/g;
 $name_lowercase =~ tr/[A-Z]/[a-z]/;
 $name_lowercase =~ s/^_//;
 
-my $leg_type = "biped";
+$leg_type = "biped";
 
-if ( $preset eq "taur" ) {
+if ( $preset eq "taurLike" ) {
 	$leg_type = "centaur";
 }
 
-if ( $preset eq "snake" || $preset eq "leglessShark" || $preset eq "leglessMantaRay" ) {
+if ( $preset eq "snakeLike" || $preset eq "leglessShark" || $preset eq "leglessMantaRay" ) {
 	$leg_type = "legless";
 }
 
@@ -184,10 +202,11 @@ for( my $i = 1; $i<scalar(@mapped_file); $i++) {# {{{
 
 	if ( $doneExtend == 0 && $mapped_file[$i] =~ /public class/ ) {
 		$mapped_file[$i] =~ s/<T extends Entity> extends EntityModel<T>/$EXTEND/;
-		splice(@mapped_file, $i-1, 0, '@OnlyIn(Dist.CLIENT)');
 
 		$i++;
+		splice(@mapped_file, $i, 0, $DECLARATIONS);
 		splice(@mapped_file, $i, 0, $GENERIC_OVERRIDES);
+		splice(@mapped_file, $i-2, 0, '@OnlyIn(Dist.CLIENT)');
 		$doneExtend = 1;
 		next;
 	}
@@ -195,12 +214,6 @@ for( my $i = 1; $i<scalar(@mapped_file); $i++) {# {{{
 	$mapped_file[$i] =~ s/new ResourceLocation\("modid", "[a-z]+"\),/AdditionalTransfurs.modResource\("entity\/$name_lowercase"\),/;
 	if ( $inModelParts == 0 && $mapped_file[$i] =~ /private final ModelPart;/ ) {
 		$inModelParts = 1;
-		next;
-	}
-
-	if ( $inModelParts == 1 ) {
-		splice(@mapped_file, $i, 0, $DECLARATIONS);
-		$inModelParts = -1;
 		next;
 	}
 
@@ -237,54 +250,53 @@ foreach (@mapped_file) {
 	$_ =~ s/\/\*PERL_NAME_LOWERCASE\*\//$name_lowercase/g;
 	$_ =~ s/\/\*PERL_ANIMATOR_PRESET\*\//$preset/;
 	
+	if ( $preset eq "humanLike" ) {
+		$_ =~ s/\/\*PERL_LEG_STUFF\*\//$human_legs/;
+		$_ =~ s/\/\*PERL_NORMAL_TAIL_STUFF\*\///;
+	}
+
 	if ( $leg_type eq "centaur" ) {
 		$_ =~ s/\/\*PERL_IMPLEMENTS\*\//$CENTAUR_IMPLEMENTS/;
 		$_ =~ s/\/\*PERL_LEG_OVERRIDES\*\//$CENTAUR_LEG_OVERRIDES/;
 
-		$_ =~ s/\/\*PERL_ABDOMEN_STUFF\*\//$centaur_legs/
+		$_ =~ s/\/\*PERL_ABDOMEN_STUFF\*\//$centaur_legs/;
 
-		$_ =~ s/\h\)\);$//;
+                $_ =~ s/\/\*PERL_ADDITIONAL_ANIMATORS\*\//$centaur_additional_animators/;
 
 	}
 
 	if ( $leg_type eq "legless" ) {
 		$_ =~ s/\/\*PERL_IMPLEMENTS\*\//$LEGLESS_IMPLEMENTS/;
 		$_ =~ s/\/\*PERL_LEG_OVERRIDES\*\//$LEGLESS_LEG_OVERRIDES/;
-		$_ s/\/\*PERL_ABDOMEN_STUFF\8\//$legless_abdomen/
+		$_ =~ s/\/\*PERL_ABDOMEN_STUFF\*\//$legless_abdomen/;
+		$_ =~ s/\/\*PERL_NORMAL_TAIL_STUFF\*\//$normal_tail_stuff/;
 
 	}
 
 	if ( $leg_type eq "biped" ) {
 		$_ =~ s/\/\*PERL_IMPLEMENTS\*\///;
 		$_ =~ s/\/\*PERL_LEG_OVERRIDES\*\//$BIPED_LEG_OVERRIDES/;
+		$_ =~ s/\/\*PERL_NORMAL_TAIL_STUFF\*\//$normal_tail_stuff/;
+		$_ =~ s/\/\*PERL_LEG_STUFF\*\//$biped_legs/;
 
 	}
 
 
 	if ( $_ =~ /\/\*PERL_ANIMATED_EARS\*\// ) {
-		if ( $preset eq "wolf" || $preset eq "cat" || $preset eq "Deer" ) {
+		if ( $preset eq "wolfLike" || $preset eq "catLike" || $preset eq "deerLike" || $preset eq "taurLike") {
 			$_ =~ s/\/\*PERL_ANIMATED_EARS\*\//$ears/;
 		}
+
 		$_ =~ s/\/\*PERL_ANIMATED_EARS\*\///;
 	}
 
-	if ( $_ =~ /\/\*PERL_LEG_STUFF\*\// {
-		if ( $leg_type eq "biped" ) {
-			
-		}
-	}
-
-	if ( $_ =~ /\/\*PERL_ABDOMEN_STUFF\8\// {
-		if ( $leg_type eq "legless" ) {
-		}
-
-		if ( $leg_type eq "centaur" ) {
-		}
-
-	}
-
 	if ( $_ =~ /\/\*PERL_WINGED_STUFF\*\// ) {
-
+		if ( $preset eq 'birdLike' ) {
+			$_ =~ s/\/\*PERL_WINGED_STUFF\*\//$bird_wings/;
+		}
+		if ( $preset eq 'wingedDragonLike' ) {
+			$_ =~ s/\/\*PERL_WINGED_STUFF\*\//$dragon_wings/;
+		}
 	}
 
 	$_ =~ s/\/\*PERL_TAIL_PARTS_ARRAY\*\//$tail_parts_serialized/;
